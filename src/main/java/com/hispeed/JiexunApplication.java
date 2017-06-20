0b7ab41505df57634b6555288fe3889b8c6eb14f
邓天广 <dengtianguang@dengtianguangdeMacBook-Pro.local>
2017-06-20 10:56:22 +0800

    Initial commit

---
+++ src/main/java/com/hispeed/JiexunApplication.java
@@ -0,0 +1,14 @@
+package com.hispeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiexunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiexunApplication.class, args);
+
+		System.out.println("Springboot start success!!!!!");
+	}
+}
